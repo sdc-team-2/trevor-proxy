e8ba295bff21134bff8d0551dd08f0d236b68c9e
Sarkis-S <Sarkis.Sos19@gmail.com>
2019-06-03 01:20:58 -0700

    Minor changes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const port = 3000;
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const app = express();
 
 app.use(cors());
@@ -10,6 +11,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/../public'));
+
+app.get('/api/listings/:id/reservations', (req, res) => {
+  const { id } = req.params;
+  axios.get(`http://localhost:3002/api/listings/${id}/reservations`)
+    .then(response=> res.send(response.data))
+    .catch(err => {
+      console.log('ERROR: ', err);
+      res.json(err)
+    });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
