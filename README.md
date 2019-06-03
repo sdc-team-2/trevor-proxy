4716cf9db9869ac2a9fc4baf68251fbc445a6cba
Sarkis-S <Sarkis.Sos19@gmail.com>
2019-06-03 00:22:12 -0700

    More styling for front page

--- README.md
+++
@@ -1,1 +0,0 @@
-# 5arki5-Proxy
--- server/index.js
+++ server/index.js
@@ -11,42 +11,6 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/../public'));
 
-app.get('/homes', function(req, res) {
-  res.status(200);
-  db.getHomesForServer(function(home) {
-    res.json(home);
-  });
-});
-
-app.get('/reviews', function(req, res) {
-  db.Review.find({}, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(result);
-    }
-  })
-  .catch(err => {
-    console.error("error ", err);
-    res.status(400).json({err});  
-  }); 
-});
-
-app.get('/related', (req, res) => {
-  //trigger database query for 12 entries
-  //sorting and establishing of relation should be handled within the function that makes the query
-  let thisHome = req.body
-  getRelatedHomes(thisHome, (err, result) => {
-    if(err) {
-      console.log(err)
-      res.status(403).send(err)
-    } else {
-      // sends the sorted results back to the client
-      res.status(200).send(result)
-    }
-  })
-}) 
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
